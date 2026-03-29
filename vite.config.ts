17831aa0ea7b2f6ee221e10f55d832cd7654a2e8
ahmetaltuntecim <133572228+ahmetaltuntecim@users.noreply.github.com>
2026-03-29 19:08:44 +0300

    chore: remove temp log

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-if (process.env.POSTGRES_URL_NON_POOLING) {
-  console.log("================================================");
-  console.log("ESKI_VERITABANI_URL:", process.env.POSTGRES_URL_NON_POOLING);
-  console.log("================================================");
-}
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
